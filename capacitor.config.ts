0b1a8b4b1cf0b37a585dddcf69fee8050274d62e
Najam-Us-Saqib <Saqib92@users.noreply.github.com>
2023-04-14 15:53:19 +0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cam-pre',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
